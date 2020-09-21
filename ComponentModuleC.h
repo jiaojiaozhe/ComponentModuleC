3ed4f8de2ea4437b88765462ca1256f6314e398e
lanbiao <lanbiao@zhihuihutong.com>
2020-09-21 18:17:06 +0800

    组件module资源优化

---
+++ ComponentModuleC.h
@@ -0,0 +1,16 @@
+//
+//  ComponentModuleC.h
+//  ComponentModuleC
+//
+//  Created by 兰彪 on 2020/9/21.
+//
+
+#import <LogicBaseComponentModule/LogicBaseComponentModule-umbrella.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComponentModuleC : ModuleBundle
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ComponentModuleC.m
@@ -0,0 +1,12 @@
+//
+//  ComponentModuleC.m
+//  ComponentModuleC
+//
+//  Created by 兰彪 on 2020/9/21.
+//
+
+#import "ComponentModuleC.h"
+
+@implementation ComponentModuleC
+
+@end
